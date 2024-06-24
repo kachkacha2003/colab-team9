cfbdb057935ced933893d09a67abefefa6860d00
Igres111 <sergikaralashvili@gmail.com>
2024-06-25 00:44:22 +0400

    wip

--- src/component/PrivateInfo.tsx
+++ src/component/PrivateInfo.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import NextButton from "../buttons/NextButton";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type Inputs = {
   name: string;
@@ -12,12 +14,23 @@ type Inputs = {
 };
 
 function PrivateInfo() {
+  const schema = yup.object({
+    name: yup.string().required(),
+    lastName: yup.string().required(),
+    files: yup.string().required(),
+    aboutme: yup.string().required(),
+    email: yup.string().required(),
+    phone: yup.string().required(),
+  });
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
+
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -25,16 +38,20 @@ function PrivateInfo() {
       <span>1/3</span>
       <label htmlFor="name">სახელი</label>
       <input type="text" id="name" {...register("name")}></input>
+      <span>მინიმუმ 2 ასო, ქართული ასოები</span>
       <label htmlFor="lastName">გვარი</label>
       <input type="text" id="lastName" {...register("lastName")}></input>
+      <span>მინიმუმ 2 ასო, ქართული ასოები</span>
       <label htmlFor="files">პირადი ინფოს ატვირთვა</label>
       <input type="file" id="files" {...register("files")}></input>
       <label htmlFor="aboutme">ჩემ შესახებ (არასავალდებულო)</label>
       <input type="text" id="aboutme" {...register("aboutme")}></input>
       <label htmlFor="mail">ელ.ფოსტა</label>
       <input type="email" id="mail" {...register("email")}></input>
+      <span>უნდა მთავრდებოდეს @redberry.ge-ით</span>
       <label htmlFor="phone">მობილურის ნომერი</label>
       <input type="tel" id="phone" {...register("phone")}></input>
+      <span>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</span>
       <NextButton />
     </form>
   );
