b54bfe13a4cb149ce4bf1e5126bf74061fc6139f
Igres111 <sergikaralashvili@gmail.com>
2024-06-24 23:47:31 +0400

    wip ContextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     },
   ]);
   return (
-    <div>
+    <>
       <RouterProvider router={router} />
-    </div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import ContextAPI from "./logic/ContextAPI";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <App />
+    <ContextAPI />
   </React.StrictMode>
 );
