5d78a5c3a4f0ce268f194bd007a476085bfa8b2f
zura tetrashvili <zuratetra@gmail.com>
2024-06-25 00:11:44 +0400

    add employee inpormation

--- src/components/EnterPersonalData.tsx
+++ src/components/EnterPersonalData.tsx
@@ -19,6 +19,15 @@ export default function EnterPersonalData() {
             />
             <span className="text-sm font-light	mt-2">მინიმუმ 2 სიმბოლო</span>
           </form>
+          <form className="mt-3">
+            <p className="font-medium">თანამდებობა</p>
+            <input
+              type="text"
+              placeholder="დეველოპერი, დიზაინერი, ა.შ."
+              className="w-full px-4 py-[14px] mt-2 border outline-none"
+            />
+            <span className="text-sm font-light	mt-2">მინიმუმ 2 სიმბოლო</span>
+          </form>
         </div>
       </div>
     </div>
