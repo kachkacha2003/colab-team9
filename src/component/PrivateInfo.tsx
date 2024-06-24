137ef7d2724dfc59e9a84985c17f917c5490a2c1
Igres111 <sergikaralashvili@gmail.com>
2024-06-25 01:29:33 +0400

    wip finished validations

--- src/component/PrivateInfo.tsx
+++ src/component/PrivateInfo.tsx
@@ -4,23 +4,51 @@ import NextButton from "../buttons/NextButton";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+const regexLetters = /^[\u10A0-\u10FF]+$/;
+const regexMail = /@redberry\.ge$/;
+const regexTel = /^\+995(5\d{8}|\d{9})$/;
+
 type Inputs = {
   name: string;
   lastName: string;
   files: string;
-  aboutme: string;
+  aboutme?: string;
   email: string;
   phone: string;
 };
 
 function PrivateInfo() {
   const schema = yup.object({
-    name: yup.string().required(),
-    lastName: yup.string().required(),
+    name: yup
+      .string()
+      .required()
+      .min(2, "მინიმუმ 2 ასო, ქართული ასოები")
+      .test("შეცდომა-სახელი", "მინიმუმ 2 ასო, ქართული ასოები", (value) =>
+        regexLetters.test(value)
+      ),
+    lastName: yup
+      .string()
+      .required()
+      .min(2, "მინიმუმ 2 ასო, ქართული ასოები")
+      .test("შეცდომა-გვარი", "მინიმუმ 2 ასო, ქართული ასოები", (value) =>
+        regexLetters.test(value)
+      ),
     files: yup.string().required(),
-    aboutme: yup.string().required(),
-    email: yup.string().required(),
-    phone: yup.string().required(),
+    aboutme: yup.string(),
+    email: yup
+      .string()
+      .required()
+      .test("შეცდომა-ფოსტა", "უნდა მთავრდებოდეს @redberry.ge-ით", (value) =>
+        regexMail.test(value)
+      ),
+    phone: yup
+      .string()
+      .required()
+      .test(
+        "შეცდომა-ტელეფონი",
+        "უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს",
+        (value) => regexMail.test(value)
+      ),
   });
   const {
     register,
@@ -32,26 +60,27 @@ function PrivateInfo() {
   });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  console.log(errors.name?.message);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h1>პირადი ინფო</h1>
       <span>1/3</span>
       <label htmlFor="name">სახელი</label>
       <input type="text" id="name" {...register("name")}></input>
-      <span>მინიმუმ 2 ასო, ქართული ასოები</span>
+      {errors.name && <span>{errors.name.message}</span>}
       <label htmlFor="lastName">გვარი</label>
       <input type="text" id="lastName" {...register("lastName")}></input>
-      <span>მინიმუმ 2 ასო, ქართული ასოები</span>
+      {errors.lastName && <span>{errors.lastName.message}</span>}
       <label htmlFor="files">პირადი ინფოს ატვირთვა</label>
       <input type="file" id="files" {...register("files")}></input>
       <label htmlFor="aboutme">ჩემ შესახებ (არასავალდებულო)</label>
       <input type="text" id="aboutme" {...register("aboutme")}></input>
       <label htmlFor="mail">ელ.ფოსტა</label>
       <input type="email" id="mail" {...register("email")}></input>
-      <span>უნდა მთავრდებოდეს @redberry.ge-ით</span>
+      {errors.email && <span>{errors.email.message}</span>}
       <label htmlFor="phone">მობილურის ნომერი</label>
       <input type="tel" id="phone" {...register("phone")}></input>
-      <span>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</span>
+      {errors.phone && <span>{errors.phone.message}</span>}
       <NextButton />
     </form>
   );
